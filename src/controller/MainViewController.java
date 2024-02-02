0cc633ea33bef6f2259f6c6dde7c0d1c5d473c49
Anti <anti.alman@ut.ee>
2024-02-02 13:20:56 +0200

    Initial commit

---
+++ src/controller/MainViewController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class MainViewController {
+
+	private Stage stage;
+	
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+}
---
+++ src/main/MainGui.java
@@ -0,0 +1,48 @@
+package main;
+
+import controller.MainViewController;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class MainGui extends Application {
+
+	private static Scene scene;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		String fxmlPath = "MainView.fxml";
+		FXMLLoader fxmlLoader = new FXMLLoader(MainGui.class.getClassLoader().getResource(fxmlPath));
+		Parent parent = fxmlLoader.load();
+		((MainViewController)fxmlLoader.getController()).setStage(primaryStage);
+		scene = new Scene(parent);
+		//scene.getStylesheets().add("css/main.css");
+		
+		primaryStage.setTitle("multi-model-miner");
+		primaryStage.setScene(scene);
+		//Setting minimum window size to 720p
+		primaryStage.setMinWidth(1280);
+		primaryStage.setMinHeight(720);
+		//primaryStage.setMaximized(true);
+		primaryStage.show();
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent t) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+	}
+	
+}
