ba76c3727afc77809bd5a39712e37d9999f5a748
Anti Alman <anti.alman@gmail.com>
2025-04-30 12:25:47 +0200

    FXML ids in the relations tab

--- src/controller/tab/v3/TemporalTabController.java
+++ src/controller/tab/v3/TemporalTabController.java
@@ -2,8 +2,13 @@ package controller.tab.v3;
 
 import data.DiscoveredActivity;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import task.DeclareDiscoveryResult;
@@ -12,6 +17,28 @@ public class TemporalTabController {
 
 	@FXML
 	private ListView<DiscoveredActivity> activityListView;
+	@FXML
+	private CheckBox altLayoutDirectCheckBox;
+	@FXML
+	private CheckBox automatonDirectCheckBox;
+	@FXML
+	private WebView directRelationsWebView;
+	@FXML
+	private RadioButton followersRadioButton;
+	@FXML
+	private ToggleGroup amongToggleGroup;
+	@FXML
+	private RadioButton preceedersRadioButton;
+	@FXML
+	private CheckBox altLayoutAmongCheckBox;
+	@FXML
+	private CheckBox automatonAmongCheckBox;
+	@FXML
+	private WebView amongRelationsWebView;
+	@FXML
+	private Label closestPreceedersLabel;
+	@FXML
+	private Label closestFollowersLabel;
 
 	private Stage stage;
 
@@ -36,12 +63,17 @@ public class TemporalTabController {
 			}
 		};
 		activityListView.setCellFactory(cf -> new TextFieldListCell<DiscoveredActivity>(activityConverter));
+		activityListView.getSelectionModel().selectedIndexProperty().addListener((obs, oldIndex, newIndex) -> {
+			
+		});
 	}
-
 
 	public void updateTabContents(DeclareDiscoveryResult declareDiscoveryResult) {
 		activityListView.getItems().setAll(declareDiscoveryResult.getActivities());
-		
 	}
+	
+	
+	
+	
 
 }
